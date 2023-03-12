8f79da31d7211db710dff6fa36fd458695b6f4da
buihongquan135 <19522073@gm.uit.edu.vn>
2023-03-12 23:21:11 +0700

    my changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: "",
+            history: 0,
+            operated: false
+        }
+    }
+
+    handleClick = (e) => {
+        this.setState(state => {
+            if (state.operated === true) {
+                state.operated = false;
+                state.result = "";
+            }
+
+            return {result: state.result.concat(e.target.value)}
+        })
+    }
+
+    handleAdd = () => {
+        this.setState(state => {
+            const num = Number(state.result)
+            const sum = state.history + num
+            return {
+                history: sum,
+                result: String(sum),
+                operated: true
+            }
+        })
+    }
+
+    handleDel = () => {
+
+    }
+
+    handleAC = () => {
+
+    }
+
+    handleEqual = () => {
+
+    }
+
+    render() {
+        return (
+            <div className="calculator-grid">
+                <div className="output">
+                    <div className="previous-operand"></div>
+                    <div className="current-operand">{this.state.result}</div>
+                </div>
+                <button className="span-two" onClick={this.handleAC}>AC</button>
+                <button onClick={this.handleDel}>DEL</button>
+                <button>%
+                </button>
+                <button onClick={this.handleClick}
+                        value={'1'}>1
+                </button>
+                <button onClick={this.handleClick}
+                        value={'2'}>2
+                </button>
+                <button onClick={this.handleClick}
+                        value={'3'}>3
+                </button>
+                <button onClick={this.handleClick}>*</button>
+                <button onClick={this.handleClick}
+                        value={'4'}>4
+                </button>
+                <button onClick={this.handleClick}
+                        value={'5'}>5
+                </button>
+                <button onClick={this.handleClick}
+                        value={'6'}>6
+                </button>
+                <button onClick={this.handleAdd}>+</button>
+                <button onClick={this.handleClick}
+                        value={'7'}>7
+                </button>
+                <button onClick={this.handleClick}
+                        value={'8'}>8
+                </button>
+                <button onClick={this.handleClick}
+                        value={'9'}>9
+                </button>
+                <button onClick={this.handleClick}>-</button>
+                <button onClick={this.handleClick}>.</button>
+                <button onClick={this.handleClick}>0</button>
+                <button className="span-two" onClick={this.handleEqual}>=</button>
+            </div>
+        );
+    }
+}
+
+root.render(<Calculator/>);
+
